f1059ef084e44642b09d8f6ae50acb1b4068c9e6
Alexander Jung <a.jung@lancaster.ac.uk>
2017-11-07 15:30:46 +0000

    Moved exceptions to api module and started output method

--- trendt/apis/api.py
+++ trendt/apis/api.py
@@ -1,11 +1,21 @@
 class api:
+    # Initialise the API with program arguments
     def init(self, _args=None, _parser=None, _verbose=False):
         raise NotImplementedError()
 
-    def search(self, keywords=None, offset=0):
+    # Perform a search and parse results at offset
+    def search(self, _keywords=None, _offset=0):
         raise NotImplementedError()
 
-    class NoKeywordException(Exception):
-        def __init__(self, message, foo, *args):
-            self.message = 'No keyword(s) have been specified!'
-            super(api.NoKeywordsException, self).__init__(message, foo, *args)
+    def save(self):
+        raise NotImplementedError()
+
+class NoKeywordError(Exception):
+    def __init__(self, message, foo, *args):
+        self.message = 'No keyword(s) have been specified!'
+        super(api.NoKeywordsException, self).__init__(message, foo, *args)
+
+class MissingOAuthToken(Exception):
+    def __init__(self, message, foo, *args):
+        self.message = 'You have not provided an OAuth token for GitHub!'
+        super(api.NoKeywordsException, self).__init__(message, foo, *args)
