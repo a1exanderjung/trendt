002867c608b05fbeaf3971cf4c83d39b2f7f6da5
Alexander Jung <a.jung@lancaster.ac.uk>
2017-11-10 14:49:01 +0000

    Finalised GitHub api and added fail-safe for ratelimiting.

--- trendt/apis/github.py
+++ trendt/apis/github.py
@@ -1,31 +1,25 @@
-import requests
-import urllib
-from .api import api, NoKeywordError, MissingOAuthToken
+import requests, re, urllib, time
+from dateutil.parser import parse
+from tqdm import tqdm
+from datetime import datetime, timedelta
+from .api import api, _date_format, output_dir, NoKeywordError, MissingOAuthToken
 
+from halo import Halo
 class github(api):
-    args = None
-    parser = None
-    verbose = False
-    search_apis = {
-        'commits': {
-            'url': 'https://api.github.com/search/commits',
-            'params': {
-                'q': '%s'
-            }
-        }
-    }
+    name = 'GitHub'
+    total_count = 0
+    max_offset = 0
+    url = 'https://api.github.com/search/commits'
+    epoch_start = '2000-01-01'
     pagination_params = {
         'order': 'asc',
         'per_page': 100,
         'page': 1
     }
 
-    # Initialise the GitHub API
-    def init(self, _args=None, _parser=None, _verbose=False):
-        # Thanks for the vars -- we'll keep those!
-        self.args = _args
-        self.parser = _parser
-        self.verbose = _verbose
+    # Add additional arguments for the GitHub API
+    def register(self, _args = None, _parser = None):
+        super(github, self).register(_args, _parser)
 
         # Bespoke API arguments
         self.parser.add_argument(
@@ -40,48 +34,99 @@ class github(api):
             action='store_true'
         )
 
-        # (bad) Re-parse args to get oauth token
-        self.args = vars(self.parser.parse_args(_args))
-
-    # Search GitHub
-    def search(self, _keywords=None, _from=None, _to=None, _offset=0):
-        if _keywords is None:
-            raise NoKeywordsException
+    # Initialise the GitHub API
+    def init(self):
+        super(github, self).init()
 
         # If this method has been called, we need the oauth token
         if self.args['github_oauth_token'] is None:
             raise MissingOAuthToken
 
-        # Update offset
-        _pagination = self.pagination_params
-        _pagination['page'] = _offset
+    # Search GitHub
+    def search(self, _keywords = None, _from = None, _to = None, _offset = 0):
+        if _keywords is None:
+            raise NoKeywordsException
 
-        # Update keywords
-        _search = self.search_apis['commits']
-        _query = _search['params']['q'] % _keywords
+        # Enable a search range
+        if _from is None:
+            _from = parse(self.epoch_start)
+        elif _to is None:
+            _to = datetime.now()
 
-        # Enable ranges if specified
-        if _from is not None and _to is not None:
-            _query = 'committer-date:' + _from + '..' + _to + '+' + _query
-        elif _from is not None:
-            _query = 'committer-date:>=' + _from + '+' + _query
-        elif _to is not None:
-            _query = 'committer-date:<=' + _to + '+' + _query
+        _diff = (_to - _from).days
 
-        _search['params']['q'] = _query
+        print('Requesting results...')
+        for i in tqdm(range(_diff)):
+            _date = _from.strftime(_date_format)
 
-        # Create final search query
-        params = urllib.urlencode(dict(_pagination.items() + _search['params'].items()))
+            self.request(_keywords, _date)
+            self.save()
 
-        # GitHub conventions.../
-        params = params.replace('%3A', ':')
-        params = params.replace('%2B', '+')
+            _from = _from + timedelta(days = 1)
 
-        r = requests.get(_search['url'] + '?' + params, headers={
+    # Make a particular request to the API for a particular day
+    def request(self, _keywords = None, _date = None, _offset = 0):
+        pagination = self.pagination_params
+        pagination['page'] = _offset + 1 # offset = 0 but page = 1
+        query = _keywords
+
+        # Request for a particular date
+        if _date is not None:
+            query = 'committer-date:' + _date + '..' + _date + '+' + query
+
+        query = dict(pagination.items() + {'q' : query}.items())
+        response = self.go(self.url, query)
+        raw_data = response.json()
+
+        self.collect(_date = _date, _total = int(raw_data['total_count']))
+
+    # Make an actual request
+    def go(self, _url = None, _params = {}):
+        # Serialise the final search query
+        request = _url + '?' + urllib.urlencode(_params)
+
+        # GitHub conventions...
+        request = request.replace('%3A', ':')
+        request = request.replace('%2B', '+')
+
+        if self.verbose:
+            print('Requesting %s...' % request)
+
+        # Make the request!
+        response = requests.get(request, headers = {
             'Accept': 'application/vnd.github.cloak-preview',
             'Authorization': 'token ' + self.args['github_oauth_token']
         })
+        raw_data = response.json()
 
-        raw_data = []
+        # We've probably hit a rate limit
+        if 'items' not in raw_data.keys():
+            reset_at = datetime.fromtimestamp(float(response.headers['X-RateLimit-Reset']))
 
-        print(r.json())
+            # Wait until reset
+            diff = (reset_at - datetime.now()).total_seconds()
+
+            if diff > 0:
+                # + 5 for good measure
+                diff = int(diff + 5)
+                countdown_text = 'Waiting %d seconds due to rate limiting...'
+
+                ratelimit = Halo(
+                    text = countdown_text % diff,
+                    spinner = 'dots'
+                )
+                ratelimit.start()
+
+                for i in range(diff):
+                    ratelimit.text = countdown_text % (diff - i)
+                    try:
+                        time.sleep(1)
+                    except IOError:
+                        pass
+
+                ratelimit.stop()
+
+            # Try again?
+            response = self.go(_url, _params)
+
+        return response
