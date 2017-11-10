b536b9654f9846044dade71cbb31309450297e11
Alexander Jung <a.jung@lancaster.ac.uk>
2017-11-10 14:57:47 +0000

    APIs not complete: adding to different branch

--- trendt/apis/stackoverflow.py
+++
@@ -1,8 +0,0 @@
-from .api import api
-
-class stackoverflow(api):
-    def init(self, _args=None, _parser=None, _verbose=False):
-        pass
-
-    def search(self, _keywords=None, _from=None, _to=None, _offset=0):
-        pass
