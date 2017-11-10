03e1756388d305a3c176e844719eca6c05444886
Alexander Jung <a.jung@lancaster.ac.uk>
2017-11-10 14:59:52 +0000

    Included bitbucket api boilerplate class

---
+++ trendt/apis/bitbucket.py
@@ -0,0 +1,8 @@
+from .api import api
+
+class bitbucket(api):
+    def init(self, _args=None, _parser=None, _verbose=False):
+        pass
+
+    def search(self, _keywords=None, _from=None, _to=None, _offset=0):
+        pass
